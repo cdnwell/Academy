04545ac2a4257ca7cdcce49ea27bca108ec30db0
cdnwell <102727356+cdnwell@users.noreply.github.com>
2022-04-20 08:47:19 +0900

    Add files via upload

--- day05/ArrayQuest04.java
+++ day05/ArrayQuest04.java
@@ -10,7 +10,7 @@ public class ArrayQuest04 {
 		 * 배열 arr에 있는 모든 알파벳을 대문자로 변경 후 출력
 		 */
 		
-		char A= 'A';		//
+		char A= 'A';		//아크시 코드 값 65
 		char a = 'a';		//아스키 코드 값 97
 		char space= ' ';	//코드 값 32
 		//대문자로 바꿀때 -32
@@ -26,8 +26,10 @@ public class ArrayQuest04 {
 				//'a'는 값 97로 저장된다.
 				//arr[i] <= 'z'도 추가해주어야 밖의 글자를 계산안할 수 있다.
 				
-				arr[i]-=32;
+				//arr[i]-=32;
 				// -=을 하면 형변환을 안한다.
+			
+				arr[i]= (char)(arr[i]-32);
 				
 			}
 			
@@ -39,7 +41,8 @@ public class ArrayQuest04 {
 		
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]<='Z' && arr[i]>='A')
-				arr[i]+=32;
+				//arr[i]+=32; (가능)
+				arr[i]=(char)(arr[i]+32);
 		}
 		
 		System.out.println(Arrays.toString(arr));
